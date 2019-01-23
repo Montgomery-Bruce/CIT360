4df10d2bf0c12c6574a33f409eba2f1e6f7ddccc
brucem <brucem@IDA-WS-051.mshome.net>
2019-01-23 16:10:38 -0700

    Added Executor Class

--- src/cit360/CIT360.java
+++ src/cit360/CIT360.java
@@ -1,9 +1,12 @@
 package cit360;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
 class MyExtendedThread extends Thread{
     public void run(){
        try {
-        for(int i=0; i<10; i++){
+        for(int i=0; i<5; i++){
             
         Thread.sleep(400);
         System.out.println(Thread.currentThread().getId() + "My Extended Thread Running " + i); //shows which thread is printing
@@ -19,7 +22,7 @@ class MyRunnableThread implements Runnable {
 
      public void run() {
          try {
-        for(int i=0; i<10; i++){
+        for(int i=0; i<5; i++){
             
         Thread.sleep(400);
         System.out.println(Thread.currentThread().getId() + "My Runnable Thread Running " + i); //shows which thread is printing
@@ -32,6 +35,21 @@ class MyRunnableThread implements Runnable {
         }    
     }
 
+class MyExecutorThread implements Runnable {
+
+     public void run() {
+         try {
+        for(int i=0; i<5; i++){
+            
+        Thread.sleep(400);
+        System.out.println(Thread.currentThread().getName() + "My Executor Thread Running "); //shows which thread is printing
+        }
+        }catch (InterruptedException exc){
+        exc.printStackTrace();
+        }   
+           
+    }    
+}
 
 
 public class CIT360 {
@@ -46,10 +64,23 @@ public class CIT360 {
        Thread runnableThread = new Thread(runnableObject); //Now a thread called runnableThread is contstructed from the runnableObject
        runnableThread.start(); //Start execution of the thread
        
-       MyExtendedThread  extendedThread2 = new MyExtendedThread();
+       MyExtendedThread  extendedThread2 = new MyExtendedThread(); //Creates a 2nd extended thread
        extendedThread2.start();
        
-      /*
+       ExecutorService executorThread = Executors.newFixedThreadPool(3); //create the pool of threads
+       Runnable ExecutorThread1 = new MyExecutorThread(); 
+       Runnable ExecutorThread2 = new MyExecutorThread(); //Create 3 new objects of type MyExecutorThread class
+       Runnable ExecutorThread3 = new MyExecutorThread();
+       
+       executorThread.execute(ExecutorThread1); 
+       executorThread.execute(ExecutorThread2); //Submit the task for execution
+       executorThread.execute(ExecutorThread3);
+       
+       executorThread.shutdown(); //The executor service will keep listening for new tasks until it is explicitly shutdown
+       
+       
+       
+      
        do {
           System.out.print(".");
           try {
@@ -60,7 +91,7 @@ public class CIT360 {
           }
           
           }while (extendedThread1.isAlive() || runnableThread.isAlive() || extendedThread2.isAlive());
-      */   
+         
        System.out.println("Main thread ending.");
     
     } 
