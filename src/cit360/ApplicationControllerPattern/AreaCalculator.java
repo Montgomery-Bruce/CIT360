db101ff4f5a3cfa6ac7d1d39d02bde1a060a4778
brucem <brucem@IDA-WS-051.mshome.net>
2019-02-03 17:00:33 -0700

    Clean up some variables that were not needed.

--- src/cit360/ApplicationControllerPattern/AreaCalculator.java
+++ src/cit360/ApplicationControllerPattern/AreaCalculator.java
@@ -5,12 +5,7 @@ public class AreaCalculator {
 
     public static void main(String[] args) {
         ApplicationControllerPat appController = new ApplicationControllerPat();
-                String triangle = "triangle";
-                String parrelelogram = "parrelelogram";
-                String rightcylinder = "rightcylinder";
-                String rectangle = "rectangle";
-                Double num1;
-                Double num2;
+
                 String shape;
                 
                 Scanner in = new Scanner(System.in);
