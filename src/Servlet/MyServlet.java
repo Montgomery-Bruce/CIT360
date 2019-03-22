780964d5fa7c439a3333556d214cfb0f6ef00b06
brucem <brucem@IDA-WS-051.mshome.net>
2019-03-22 12:17:00 -0600

    Completed Servlet Connection

---
+++ src/Servlet/MyServlet.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author brucem
+ */
+@WebServlet(name = "MyServlet", urlPatterns={"/MyServlet"})
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+             
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Access Form data with post method</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<b>Welcome " + request.getParameter("name") + "!" + "</b><br>");
+            out.println("<b>Age " + request.getParameter("age") + "</b><br>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("This resource is not available via get method");
+            
+        }
+    }
+
+}
